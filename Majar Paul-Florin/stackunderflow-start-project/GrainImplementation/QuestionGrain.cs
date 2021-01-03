a53f1c3e805a03bff5229d204702c92d56869b7f
Majar Paul-Florin <62726072+pauls119@users.noreply.github.com>
2021-01-03 09:38:42 -0800

    Add files via upload

---
+++ Majar Paul-Florin/stackunderflow-start-project/GrainImplementation/QuestionGrain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using StackUnderflow.Domain.Schema.Models;
+using StackUnderflow.EF.Models;
+
+namespace GrainImplementation
+{
+    public class QuestionGrain : Orleans.Grain
+    {
+        private StackUnderflowContext _dbContext;
+        private QuestionSummary state;
+        private int questionId;
+
+        public QuestionGrain(int questionId, StackUnderflowContext dbContext)
+        {
+            this.questionId = questionId;
+            _dbContext = dbContext;
+        }
+
+        public override Task OnActivateAsync()
+        {
+            //read state from DB
+            //where PostId= OR ParentId=
+            state = (QuestionSummary)_dbContext.Set<QuestionSummary>().Where(element => element.QuestionId == questionId);
+            
+            //subscribe to replys stream
+            GetStreamProvider(state.ToString());
+            
+            return base.OnActivateAsync();
+        }
+
+        public QuestionSummary GetQuestionWithReplys()
+        {
+            return state;
+        }
+    }
+}
