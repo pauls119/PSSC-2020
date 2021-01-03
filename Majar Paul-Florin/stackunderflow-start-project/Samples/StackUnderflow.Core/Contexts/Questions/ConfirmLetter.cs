17b166cdd541d8bfece261601d497a8d355ae4d6
Majar Paul-Florin <62726072+pauls119@users.noreply.github.com>
2021-01-03 08:22:52 -0800

    Add files via upload

---
+++ Majar Paul-Florin/stackunderflow-start-project/Samples/StackUnderflow.Core/Contexts/Questions/ConfirmLetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Questions
+{
+    public class ConfirmLetter
+    {
+        public ConfirmLetter(string email, string letter, Uri link)
+        {
+            Email = email;
+            Letter = letter;
+            ConfirmationLink = link;
+        }
+        public string Email { get; private set; }
+        public string Letter { get; private set; }
+        public Uri ConfirmationLink { get; private set; }
+    }
+}
