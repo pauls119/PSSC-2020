3eae1f56737e5a15581253257a17e3b8e042ed43
Majar Paul-Florin <62726072+pauls119@users.noreply.github.com>
2021-01-06 06:53:48 -0800

    Add files via upload

--- Majar Paul-Florin/stackunderflow-start-project/Samples/StackUnderflow.Core/Contexts/Questions/QuestionOp/CreateQuestionCmd.cs
+++ Majar Paul-Florin/stackunderflow-start-project/Samples/StackUnderflow.Core/Contexts/Questions/QuestionOp/CreateQuestionCmd.cs
@@ -1,16 +1,26 @@
 ﻿using StackUnderflow.EF.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace StackUnderflow.Domain.Core.Contexts.Questions.QuestionOp
 {
     class CreateQuestionCmd
     {
-
+        [Required(ErrorMessage = "Title is missing")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Title is not valid.")]
+        [MinLength(10), MaxLength(200)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Body is missing")]
+        [MinLength(15), MaxLength(10000)]
         public string Body { get; set; }
+
+        [Required(ErrorMessage = "TenantId is missing")]
         public int TenantId { get; set; }
+
+        [Required(ErrorMessage = "Please enter at least one tag; see a list of popular tags.")]
+        [MinLength(1), MaxLength(100)]
         public string Tag { get; set; } 
         public Guid UserId { get; set; }
         //   public User User { get; set; }
